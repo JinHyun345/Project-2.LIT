c715322ca269cc8007de10ec03d880267a474873
JinHyun345 <kimjh0957@gmail.com>
2025-03-16 15:59:33 +0900

    Add files via upload
    tag의 CRUD 구현 다 했는데 주소에 데이터가 표시되어서 라우팅 수정 중...
    memo의 Create 구현 완료, Read는 정렬 하는 기능 추가되어야 함. Update와 Delete는 미완
    집컴에서 구현할 때 memo에는 데이터 좀 넣어두고 시작하셈!

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ const port = 3000;
 const tem = require('./lib/tem.js');
 const path = require('path');
 const mysql = require('mysql2');
+const url = require('url');
+var bodyParser = require('body-parser')
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '#%gimco682GILgodarling',
-    database: 'mybox'
+    database: 'mybox',
+    dateStrings: "date"
 });
 db.connect();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
     var body = `
@@ -26,12 +30,32 @@ app.get('/', (request, response) => {
     response.send(tem.html(body));
 });
 app.get('/memo', (request, response) => {
-    var body = `
+    db.query(`SELECT * FROM memo`, (err, memos) => {
+        if (err) {
+            throw err;
+        }
+        var body = `
         <h3>
             <a href = '/memo/create'>+ new memo</a>
         </h3>
-    `;
-    response.send(tem.html(body));
+        <p>Click title to edit memo</p>
+        `;
+        var list = '<ul>';
+        var i = 0;
+        while (i < memos.length) {
+            list = list + `<li style="list-style:none">
+            <h3><a href='/memo/update'${memos[i].title}</h3>
+            <h4 style="display:inline">${memos[i].type}</h4>
+            <h5 style="display:inline">: ${memos[i].tag}</h5>
+            <p>${memos[i].text}</p>
+            <p>${memos[i].date}</p>
+            </li>`;
+            i = i + 1;
+        }
+        list = list + '</ul>';
+        var body = body + list + '<br><br>';
+        response.send(tem.html(body));
+    });
 })
 app.get('/memo/create', (request, response) => {
     var body = `
@@ -51,32 +75,47 @@ app.get('/memo/create', (request, response) => {
     response.send(tem.html(body));
 })
 app.get('/memo/create/:type', (request, response) => {
-    var type = request.params.type;
-    var body = '';
-    if (type === 'learning') {
-        body += '<h2>Today I learned...</h2>';
-    } else if (type === 'idea') {
-        body += '<h2>I have a new idea!</h2>'
-    } else {
-        body += `<h2>Today I'm thankful for...</h2>`
-    }
-    body += `
-    <form action="/memo/create/${type}/process">
-        <input type="hidden" name="type" value="${type}">
-        <select name="tag">
-            <option value="reading">reading</option>
-            <option value="coding">coding</option>
-            <option value="relationship">relationship</option>
-            <option value="life">life</option>
-        </select>
-        <input name="title" placeholder="write title here..." style="display:block">
-        <textarea name="text" style="display:block"></textarea>
-        <input type="submit" value="Done">
-    </form>
-    `;
-    response.send(tem.html(body));
+    db.query('SELECT * FROM tag', (err, tags) => {
+        var type = request.params.type;
+        var body = '';
+        if (type === 'learning') {
+            body += '<h2>Today I learned...</h2>';
+        } else if (type === 'idea') {
+            body += '<h2>I have a new idea!</h2>'
+        } else {
+            body += `<h2>Today I'm thankful for...</h2>`
+        }
+        
+        var i = 0;
+        var list = '';
+        while (i < tags.length) {
+            list = list + `<option value=${tags[i].name}>${tags[i].name}</option>`;
+            i = i + 1;
+        }
+        list = list + '</ul>';
+        var body = `
+        <form action="/memo/create/${type}/process" method ="post">
+            <input type="hidden" name="type" value="${type}">
+            <select name="tag">
+            ${list}
+            </select>
+            <input name="title" placeholder="write title here..." style="display:block">
+            <textarea name="text" style="display:block"></textarea>
+            <input type="submit" value="Done">
+        </form>`;
+        response.send(tem.html(body));
+    })
+})
+app.post('/memo/create/:type/process', (request, response)=>{
+    var body = request.body;
+    db.query('INSERT INTO memo (type, tag, title, text, date) VALUES (?,?,?,?,NOW())',
+        [body.type, body.tag, body.title, body.text], (err, result)=>{
+            response.redirect('/memo');
+        })
 })
 app.get('/tag', (request, response) => {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
     db.query(`SELECT * FROM tag`, (err, tags) => {
         if (err) {
             throw err;
@@ -84,19 +123,106 @@ app.get('/tag', (request, response) => {
         var list = '<ul>';
         var i = 0;
         while (i < tags.length) {
-            list = list + `<li>${tags[i].name}</li>`;
+            list = list + `<li><a href="/tag/edit">${tags[i].name}</a></li>`;
             i = i + 1;
         }
         list = list + '</ul>';
         var body = `
         <form action="/tag/create" method="post">
-            <input type="text" id="tag" placeholder="Add new tag here">
+            <input type="text" name="name" placeholder="Add new tag here">
             <input type="submit" value="+">
         </form>
+        <p>Click tag to edit</p>
         ${list}
     `;
-    response.send(tem.html(body));
+        response.send(tem.html(body));
     });
+})
+app.get('/tag/edit', (request, response) => {
+    db.query(`SELECT * FROM tag`, (err, tags) => {
+        if (err) {
+            throw err;
+        }
+        var tagname = request.params.name;
+        var list = '<ul>';
+        var i = 0;
+        while (i < tags.length) {
+            if (tags[i].name === tagname) {
+                list = list + `<li>
+                <form action="/tag/edit/update" method="post" style="display:inline">
+                    <input type="hidden" name="id" value="${tags[i].name}">
+                    <input type="text" name="name" value="${tags[i].name}"><input type="submit" value="OK">
+                </form>
+                <form action="/tag/edit/delete" method="post" style="display:inline">
+                <input type="hidden" name="id" value="${tags[i].name}">
+                <input type="submit" value="X">
+                </form>
+                </li>`;
+            }
+            else {
+                list = list + `<li><a href="/tag/${tags[i].name}">${tags[i].name}</a></li>`;
+            }
+            i++;
+        }
+        list = list + '</ul>';
+        var body = `
+        <form action="/tag/create" method="post">
+            <input type="text" name="name" placeholder="Add new tag here">
+            <input type="submit" value="+">
+        </form>
+        <p>Click tag to edit</p>
+        ${list}
+    `;
+        response.send(tem.html(body));
+    });
+})
+app.post('/tag/create', (request, response) => {
+    var name = request.body.name;
+    db.query(`SELECT * FROM tag WHERE name=?`, [name], (err1, tag) => {
+        if (err1) {
+            throw err1;
+        }
+        if (tag.length === 0) {
+            db.query(`INSERT INTO tag (name, date) VALUES (?, NOW())`, [name], (err2, result2) => {
+                if (err2) {
+                    throw err2;
+                }
+                db.query(`set @cnt = 0`, (err2, results2) => {
+                    db.query(`UPDATE tag set tag.id = @cnt:=@cnt+1`, (err3, results3) => {
+                        response.redirect('/tag');
+                    })
+                })
+            })
+        }
+        else {
+            response.send(`<script>alert('Already exist!'); window.location.href='/tag';</script>`);
+        }
+    })
+
+})
+app.post('/tag/edit/update', (request, response) => {
+    var id = request.body.id;
+    var name = request.body.name;
+    db.query(`UPDATE tag SET name=? WHERE name =?`, [name, id], (err1, results1) => {
+        if (err1) {
+            throw err1;
+        }
+        response.redirect('/tag');
+    })
+})
+app.post('/tag/edit/delete', (request, response) => {
+    var id = request.body.id;
+    db.query(`DELETE FROM tag WHERE name =?`, [id], (err2, results) => {
+        if (err2) {
+            throw err2;
+        }
+        db.query(`set @cnt = 0`, (err2, results2) => {
+            db.query(`UPDATE tag set tag.id = @cnt:=@cnt+1`, (err3, results3) => {
+                response.redirect('/tag');
+            })
+        })
+    })
+
 })
 
 app.listen(port, () => { });
